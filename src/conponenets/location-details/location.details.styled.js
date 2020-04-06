a5b5b1ce3077c9f517376f002c0e8a4ecf511bf8
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-04-06 19:32:05 +0100

    improved location details styles

--- src/conponenets/location-details/location.details.styled.js
+++ src/conponenets/location-details/location.details.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledLocation = styled.div`
+  position: relative;
   color: black;
   background: #fff;
   padding: 15px 10px;
@@ -10,10 +11,26 @@ export const StyledLocation = styled.div`
   align-items: center;
   justify-content: center;
   width: 6rem;
+  height: auto;
   margin: auto;
   border-radius: 2px;
   box-shadow: 0px 16px 24px #50b0f117;
   transform: translate(-50%, -50%);
+
+  &:after {
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -10px;
+    margin-top: -10px;
+    width: 20px;
+    height: 20px;
+    border-bottom: 2px solid #fff;
+    border-right: 2px solid #fff;
+    transform: rotate(45deg);
+    background: #fff;
+  }
 
   .long {
     margin-bottom: 0.5rem;
